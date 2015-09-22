8505d84b8c9704b608b2db39001078b7edb7441e
Justin Woo <moomoowoo@gmail.com>
2015-09-22 23:28:26 +0800

    try replacing react reviews with elm for fun

--- web/static/elm/App.elm
+++ web/static/elm/App.elm
@@ -2,4 +2,46 @@ module App where
 
 import Html exposing (..)
 
-main = text "sdf"
+type alias File = String
+type alias Files = List File
+
+type alias Model = {
+  files: Files
+}
+
+port newFiles : Signal Files
+
+updateFiles : Signal (Model -> Model)
+updateFiles =
+  Signal.map
+    (\files -> (\model -> { model | files <- files }))
+    newFiles
+
+
+update : (Model -> Model) -> Model -> Model
+update folder model =
+  folder model
+
+fileView : File -> Html
+fileView file =
+  div [] [text file]
+
+filesView : Files -> Html
+filesView files =
+  div [] (List.map (\n -> fileView n) files)
+
+view : Model -> Html
+view model =
+  div []
+    [
+      h3 [] [text "Muh Filez:"],
+      filesView model.files
+    ]
+
+model =
+  {
+    files = []
+  }
+
+main =
+  Signal.map view (Signal.foldp update model updateFiles)
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,17 +1,9 @@
 import $ from 'jquery';
 import Rx from 'rx';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Elm from '../elm/App.elm';
-Elm.fullscreen(Elm.App);
 
 var filesData$ = new Rx.Subject();
-var updateFilesData$ = filesData$.map(function (files) {
-  return function (state) {
-    return Object.assign(state, { files });
-  };
-});
 
 function loadFilesData() {
   $.get(`${window.API_ROOT}/files`, function (files) {
@@ -19,40 +11,16 @@ function loadFilesData() {
   });
 }
 
-function getState$() {
-  return Rx.Observable.merge(
-    updateFilesData$
-  ).scan(function (state, scanner) {
-    return scanner(state);
-  }, {
-    files: []
-  });
-}
-
-function FilesView(props) {
-  return (
-    <div>
-      {props.files.map(file => <div key={file}>{file}</div>)}
-    </div>
-  );
-}
-
-function view(state) {
-  return (
-    <div>
-      <h3>Muh Filez:</h3>
-      <FilesView files={state.files}/>
-    </div>
-  );
-}
-
 function init() {
   loadFilesData();
-  var appContainer = document.getElementById('app');
-  var state$ = getState$();
 
-  state$.subscribe(function (state) {
-    ReactDOM.render(view(state), appContainer);
+  var appContainer = document.getElementById('app');
+  var elmApp = Elm.embed(Elm.App, appContainer, {
+    newFiles: []
+  });
+
+  filesData$.subscribe(function (files) {
+    elmApp.ports.newFiles.send(files);
   });
 }
 
