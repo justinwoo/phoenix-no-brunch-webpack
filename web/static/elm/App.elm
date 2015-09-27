4b2bad21b82c8312dead26b6d87495a0f819e6da
Justin Woo <moomoowoo@gmail.com>
2015-09-27 10:19:16 -0500

    add list filtering using input value substring

--- web/static/elm/App.elm
+++ web/static/elm/App.elm
@@ -1,15 +1,21 @@
 module App where
 
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, on)
+import Json.Decode as Json
+import String
 
 type alias File = String
 type alias Files = List File
 
 type alias Model = {
   files: Files,
-  times: Int
+  times: Int,
+  filterPhrase: Maybe String
 }
+
+-- may change in the future
+type alias ViewModel = Model
 
 type Action =
   NoOp
@@ -23,15 +29,48 @@ port updateRequests =
 
 port newFiles : Signal Files
 
+filterPhraseInputMailbox : Signal.Mailbox String
+filterPhraseInputMailbox = Signal.mailbox ""
+
 updateFiles : Signal (Model -> Model)
 updateFiles =
   Signal.map
     (\files -> (\model -> { model | files <- files, times <- model.times + 1 }))
     newFiles
 
+updateFilterPhrase : Signal (Model -> Model)
+updateFilterPhrase =
+  Signal.map
+    (\inputValue ->
+      let
+        filterPhrase = if | not (String.isEmpty inputValue) -> Just inputValue
+                          | otherwise -> Nothing
+      in
+        (\model -> { model | filterPhrase <- filterPhrase }))
+    filterPhraseInputMailbox.signal
+
 update : (Model -> Model) -> Model -> Model
 update folder model =
   folder model
+
+applyFilter : Model -> ViewModel
+applyFilter model =
+  case model.filterPhrase of
+    Just phrase ->
+      { model |
+        files <-
+          List.filter
+            (\item -> String.contains phrase item)
+            model.files }
+    Nothing -> model
+
+computeViewModel : Model -> ViewModel
+computeViewModel model =
+  applyFilter model
+
+targetValueString : Json.Decoder String
+targetValueString =
+  Json.at ["target", "value"] Json.string
 
 updateRequestButton : Html
 updateRequestButton =
@@ -41,6 +80,14 @@ updateRequestButton =
     ]
     [text "request update"]
 
+filterPhraseInput : Html
+filterPhraseInput =
+  input
+    [
+      on "keyup" targetValueString (\value -> Signal.message filterPhraseInputMailbox.address value)
+    ]
+    []
+
 fileView : File -> Html
 fileView file =
   div [] [text file]
@@ -49,12 +96,17 @@ filesView : Files -> Html
 filesView files =
   div [] (List.map (\n -> fileView n) files)
 
-view : Model -> Html
+view : ViewModel -> Html
 view model =
   div []
     [
       h3 [] [text "Muh Filez:"],
       updateRequestButton,
+      div []
+        [
+          text "filter",
+          filterPhraseInput
+        ],
       h4 [] [text ("times updated: " ++ (toString model.times))],
       filesView model.files
     ]
@@ -62,13 +114,16 @@ view model =
 model =
   {
     files = [],
-    times = 0
+    times = 0,
+    filterPhrase = Nothing
   }
 
 main =
   Signal.mergeMany
     [
-      updateFiles
+      updateFiles,
+      updateFilterPhrase
     ]
   |> Signal.foldp update model
+  |> Signal.map computeViewModel
   |> Signal.map view
